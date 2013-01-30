48d939f4839b5e3bd698adf9ca244d99ff712d27
Chris Long <christopherglong@mac.com>
2013-01-30 17:40:58 -0500

    Empty meminfo read function

--- mymeminfo/mymeminfo.c
+++ mymeminfo/mymeminfo.c
@@ -8,6 +8,8 @@ MODULE_AUTHOR("Chris Long");
 
 static struct proc_dir_entry *proc_entry;
 
+int mymeminfo_read(char *page, char **start, off_t off, int count, int *eof, void *data);
+
 int init_mymeminfo_module()
 {
     int ret = 0;
@@ -15,6 +17,7 @@ int init_mymeminfo_module()
     proc_entry = create_proc_entry("mymeminfo", 0644, NULL);
     if (proc_entry != NULL)
     {
+        proc_entry->read_proc = mymeminfo_read;
         proc_entry->owner = THIS_MODULE;
         printk(KERN_INFO "mymeminfo: Module loaded.\n");
     }
@@ -30,6 +33,11 @@ void cleanup_mymeminfo_module()
 {
     remove_proc_entry("mymeminfo", proc_entry);
     printk(KERN_INFO "mymeminfo: Module unloaded.\n");
+}
+
+int mymeminfo_read(char *page, char **start, off_t off, int count, int *eof, void *data)
+{
+    return -1;
 }
 
 module_init(init_mymeminfo_module);
