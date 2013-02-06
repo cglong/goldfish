aafc22241645852c429a6ee2c10813ebf5cf4517
Chris Long <christopherglong@mac.com>
2013-02-06 15:34:19 -0500

    Store PID to query

--- myschedinfo/myschedinfo.c
+++ myschedinfo/myschedinfo.c
@@ -7,6 +7,9 @@ MODULE_DESCRIPTION("Simplified scheduling info kernel module");
 MODULE_AUTHOR("Chris Long");
 
 static struct proc_dir_entry *proc_entry;
+long pid;
+
+ssize_t myschedinfo_write(struct file *filp, const char __user *buff, unsigned long len, void *data);
 
 int init_myschedinfo_module(void)
 {
@@ -15,6 +18,7 @@ int init_myschedinfo_module(void)
     proc_entry = create_proc_entry("myschedinfo", 0644, NULL);
     if (proc_entry != NULL)
     {
+        proc_entry->write_proc = myschedinfo_write;
         proc_entry->owner = THIS_MODULE;
         printk(KERN_INFO "myschedinfo: Module loaded.\n");
     }
@@ -30,6 +34,12 @@ void cleanup_myschedinfo_module(void)
 {
     remove_proc_entry("myschedinfo", proc_entry);
     printk(KERN_INFO "myschedinfo: Module unloaded.\n");
+}
+
+ssize_t myschedinfo_write(struct file *filp, const char __user *buff, unsigned long len, void *data)
+{
+    pid = simple_strtol(buff, NULL, 10);
+    return len;
 }
 
 module_init(init_myschedinfo_module);
