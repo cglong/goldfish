fe1d5c1b2b792c25afbb8eee1b2496c7fee600cf
Wonhee Cho <wonhee@gatech.edu>
2013-02-10 02:18:31 -0500

    Refactoring & bugfix exit

--- drivers/char/meoschrdv/meoschrdv.c
+++ drivers/char/meoschrdv/meoschrdv.c
@@ -11,101 +11,121 @@ MODULE_LICENSE("GPL");
 static int debug_enable = 0;
 module_param(debug_enable, int, 0);
 
-struct file_operations meoschrdv_fops;
-static struct meoschrdv my_dev;
-static struct class *my_dev_class = NULL;
-
-static int meoschrdv_open(struct inode *inode, struct file *file)
+struct file_operations mchr_fops;
+static struct mchr pmydev;
+static struct class *pmyclass = NULL;
+static unsigned int devno_major = 0;
+static int mchr_open(struct inode *inode, struct file *file)
 {
-  printk(KERN_INFO "Opened meoschrdv successfully.\n");
+  printk(KERN_INFO "Opened mchr successfully.\n");
   return 0;
 }
 
-static int meoschrdv_release(struct inode *inode, struct file *file)
+static int mchr_release(struct inode *inode, struct file *file)
 {
-  printk(KERN_INFO "Released meoschrdv successfully.\n");
+  printk(KERN_INFO "Released mchr successfully.\n");
   return 0;
 }
 
-static ssize_t meoschrdv_read(struct file *file, char __user *buf, size_t count, loff_t *ptr)
+static ssize_t mchr_read(struct file *file, char __user *buf, size_t count, loff_t *ptr)
 {
   printk(KERN_INFO "Read %d bytes from %p.\n", count, buf);
   return 0;
 }
 
-static ssize_t meoschrdv_write(struct file *file, const char __user *buf, size_t count, loff_t *ppos)
+static ssize_t mchr_write(struct file *file, const char __user *buf, size_t count, loff_t *ppos)
 {
   printk(KERN_INFO "Write %d bytes to %p.\n", count, buf);
   return 0;
 }
 
-struct file_operations meoschrdv_fops = {
+struct file_operations mchr_fops = {
   .owner = THIS_MODULE,
-  .read = meoschrdv_read,
-  .write = meoschrdv_write,
-  .open = meoschrdv_open,
-  .release = meoschrdv_release,
+  .read = mchr_read,
+  .write = mchr_write,
+  .open = mchr_open,
+  .release = mchr_release,
 };
 
 
-static int __init meoschrdv_init(void)
+static int mchr_setup_device(struct mchr *pmydev, struct class *pclass, dev_t devno)
+{
+  int err = 0;
+  struct device *device = NULL;
+
+  cdev_init(&pmydev->cdev, &mchr_fops);
+  pmydev->cdev.owner = THIS_MODULE;
+  pmydev->cdev.ops = &mchr_fops;
+  err = cdev_add(&pmydev->cdev, devno, MCHR_NUM_DEVICES);
+
+  if (err != 0)
+    {
+      printk(KERN_ERR "Error %d while adding %s.\n", err, MCHR_DEVICE_NAME);
+    }
+
+  
+  device = device_create(pclass, NULL,
+			 devno, NULL,
+			 MCHR_DEVICE_NAME);
+
+  if (IS_ERR(device)) {
+    err = PTR_ERR(device);
+    printk(KERN_WARNING "Error %d while trying to create %s",
+	   err, MCHR_DEVICE_NAME);
+    cdev_del(&pmydev->cdev);
+    return err;
+  }
+  return err;
+}
+
+static int __init mchr_init(void)
 {
   int err = 0;
   dev_t dev_no = 0;
-  struct device *device = NULL;
   
   printk(KERN_INFO "Moes Char Device Driver started.\n");
   printk(KERN_INFO "Currently debug mode is %s.\n", (debug_enable ? "enabled":"disabled"));
-  printk(KERN_INFO "Hello\n");
 
   // Get device numbers
-  err = alloc_chrdev_region(&dev_no, MEOSCHRDV_MINOR_BASE, MEOSCHRDV_NUM_DEVICES, MEOSCHRDV_DEVICE_NAME);
+  err = alloc_chrdev_region(&dev_no, MCHR_MINOR_BASE, MCHR_NUM_DEVICES, MCHR_DEVICE_NAME);
   if (err < 0)
     {
       printk(KERN_ERR "Device number allocation failed.\n");
       return err;
     }
 
-  // Create device class
-  my_dev_class = class_create(THIS_MODULE, MEOSCHRDV_DEVICE_NAME);
-  if (IS_ERR(my_dev_class))
-    return PTR_ERR(my_dev_class);
+  devno_major = MAJOR(dev_no);
 
-  cdev_init(&my_dev.cdev, &meoschrdv_fops);
-  my_dev.cdev.owner = THIS_MODULE;
-  my_dev.cdev.ops = &meoschrdv_fops;
-  err = cdev_add(&my_dev.cdev, dev_no, MEOSCHRDV_NUM_DEVICES);
+  // Create device class
+  pmyclass = class_create(THIS_MODULE, MCHR_DEVICE_NAME);
+  if (IS_ERR(pmyclass))
+    return PTR_ERR(pmyclass);
+
+  // Set up the device
+  err = mchr_setup_device(&pmydev, pmyclass, dev_no);
 
   if (err != 0)
     {
-      printk(KERN_ERR "Error %d while adding %s.\n", err, MEOSCHRDV_DEVICE_NAME);
+      printk(KERN_ERR "Device setup failed.\n");
+      return err;
     }
-  
-  device = device_create(my_dev_class, NULL,
-			 dev_no, NULL,
-			 MEOSCHRDV_DEVICE_NAME);
-
-  if (IS_ERR(device)) {
-    err = PTR_ERR(device);
-    printk(KERN_WARNING "Error %d while trying to create %s",
-	   err, MEOSCHRDV_DEVICE_NAME);
-    cdev_del(&my_dev.cdev);
-    return err;
-  }
-
-  printk(KERN_INFO "%s is successfully loaded.\n",MEOSCHRDV_DEVICE_NAME);
-
   return 0;
   
 }
 
-static void __exit meoschrdv_exit(void)
+static void __exit mchr_exit(void)
 {
-  cdev_del(&my_dev.cdev);
+  device_destroy(pmyclass, MKDEV(devno_major, MCHR_MINOR_BASE));
+  cdev_del(&pmydev.cdev);
+
+  if (pmyclass)
+    {
+      class_destroy(pmyclass);
+    }
 
   printk(KERN_INFO "Moes Char Device Driver Removed.\n");
   return;
 }
 
-module_init(meoschrdv_init);
-module_exit(meoschrdv_exit);
+module_init(mchr_init);
+module_exit(mchr_exit);
--- drivers/char/meoschrdv/meoschrdv.h
+++ drivers/char/meoschrdv/meoschrdv.h
@@ -1,10 +1,10 @@
 
-#define MEOSCHRDV_DEVICE_NAME "meoschrdv"
-#define MEOSCHRDV_BUFSIZE (8 * 1024) 
-#define MEOSCHRDV_NUM_DEVICES (1)
-#define MEOSCHRDV_MINOR_BASE (0)
+#define MCHR_DEVICE_NAME "mchr"
+#define MCHR_BUFSIZE (8 * 1024) 
+#define MCHR_NUM_DEVICES (1)
+#define MCHR_MINOR_BASE (0)
 
-struct meoschrdv {
+struct mchr {
   unsigned char *data;
   unsigned long bufsize;
   struct cdev cdev;
