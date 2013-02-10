35b7cbcb27ea07c49ba3018395fbbad598548762
Wonhee Cho <wonhee@gatech.edu>
2013-02-10 02:17:06 -0500

    Module parameters

--- drivers/char/meoschrdv/meoschrdv.c
+++ drivers/char/meoschrdv/meoschrdv.c
@@ -2,9 +2,16 @@
 
 MODULE_LICENSE("GPL");
 
+#define MEOSCHRDV_DEVICE_NAME "meoschrdv"
+
+static int debug_enable = 0;
+module_param(debug_enable, int, 0);
+
+
 static int __init meoschrdv_init(void)
 {
   printk(KERN_INFO "Moes Char Device Driver started.\n");
+  printk(KERN_INFO "Currently debug mode is %s.\n", (debug_enable ? "enabled":"disabled"));
   return 0;
 }
 
